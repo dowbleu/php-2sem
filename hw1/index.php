4b358f90ebe4619484e761318b74010bf92f2836
dowbleu <denfun31@gmail.com>
2025-04-12 15:08:03 +0300

    hw1

---
+++ hw1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Аргунов Денис Сергеевич 241-321 Лабораторная №1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <img src="./Logo_Polytech_rus_main.jpg" alt="Картинка">
+        <h1 class="heading">2.1. Домашняя работа: Hello, World! Время выполнения - 3 часа</h1>
+    </header>
+
+    <main>
+        <p><?php echo "Hello, World!" ?></p>
+        <p>
+            <?php echo date("l") ?>
+        </p> 
+    </main>
+
+    <footer>
+        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. </p>
+    </footer>
+</body>
+
+</html>
