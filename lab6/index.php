45f8417a21fbffaf9460f49a2af23417de1af47c
dowbleu <denfun31@gmail.com>
2025-05-24 02:41:53 +0300

    lab6 is done

--- lab6/index.php
+++ lab6/index.php
@@ -14,28 +14,37 @@
     <section class="task1">
         <h3>Задание 1 (СДО)</h3>
         <?php
-            session_start();
+        session_start();
 
-            if (!isset($_SESSION['test'])) {
-                $_SESSION['test'] = 'test';
-                echo "Данные записаны в сессию. Обнови страницу!";
-            } else {
-                echo "Содержимое сессии: " . $_SESSION['test'];
-            }
+        if (!isset($_SESSION['test'])) {
+            $_SESSION['test'] = 'test';
+            echo "Данные записаны в сессию. Обнови страницу!";
+        } else {
+            echo "Содержимое сессии: " . $_SESSION['test'];
+        }
         ?>
     </section>
 
     <section class="task2">
         <h3>Задание 2 (СДО)</h3>
         <?php
+        $_SESSION['message'] = 'куку!';
 
+        echo "Данные записаны в сессию.<br>";
+        echo '<a href="task2.php">Перейти на вторую страницу</a>';
         ?>
     </section>
 
     <section class="task3">
         <h3>Задание 3 (СДО)</h3>
         <?php
-
+        if (!isset($_SESSION['counter'])) {
+            $_SESSION['counter'] = 0;
+            echo "Йоу, чувачок, ты еще не обновил страницу. Обнови сейчас!";
+        } else {
+            $_SESSION['counter']++;
+            echo "Круто! Теперь ты обновил страницу " . $_SESSION['counter'] . " раз(а).";
+        }
         ?>
 
     </section>
@@ -43,14 +52,35 @@
     <section class="task4">
         <h3>Задание 4 (СДО)</h3>
         <?php
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $_SESSION['country'] = htmlspecialchars($_POST['country']);
+                echo "Твоя страна сохранена в сессии! Вот же она: " . $_SESSION['country'] . "<br>";
+                echo '<a href="test.php">Перейти на test.php</a>';
+            } else {
+        ?>
 
+        <form method="POST" action="">
+            <label for="country">Введи свою страну, слон:</label>
+            <input type="text" name="country" id="country" required>
+            <button type="submit">Сохранить</button>
+        </form>
+
+        <?php
+        }
         ?>
     </section>
 
     <section class="task5">
         <h3>Задание 5 (СДО)</h3>
         <?php
-
+            if (!isset($_SESSION['time'])) {
+                $_SESSION['time'] = time();
+                echo "Привет, ты только что зашел на сайт!";
+            } else {
+                $time_now = time();
+                $seconds_passed = $time_now - $_SESSION['time'];
+                echo "Ты зашел на сайт " . $seconds_passed . " секунд назад.";
+            }
         ?>
     </section>
 </body>
